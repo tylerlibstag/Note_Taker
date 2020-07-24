23cfe5baabac98318a4ca8999a9ae06d69ba4f4e
TYTY <tylerlibstag@gmail.com>
2020-07-24 14:54:37 -0400

    deploy prep

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var notesDb = require('./db/db.json')
 
 app.use(express.static('public'))
